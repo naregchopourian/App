29e253f3417fdd97924132b6c11b48f7ae59a1ca
Nareg <nareg10@outlook.com>
2016-11-11 16:24:06 -0800

    added styling to the application

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
 
-validates :title, presence: true, length: { minimum: 3, maximum: 100}
-validates :body, presence: true, length: { minimum: 3, maximum: 500}
+validates :title, presence: true
+
 
 
 
